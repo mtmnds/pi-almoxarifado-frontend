e64740b597c5c257c2af96ad279aaefb4ccbb909
Matheus Mendes <mt.mendes@outlook.com>
2020-10-18 09:01:54 -0300

    Criação do frontend do cadastro de modelo.

--- Almoxarifado/pi-almoxarifado/src/app/modules/cadastros/modelo/modelo.component.ts
+++ Almoxarifado/pi-almoxarifado/src/app/modules/cadastros/modelo/modelo.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource, MatTable } from '@angular/material/table';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModeloService } from './modelo.service';
+import { MarcaService } from '../marca/marca.service';
 
 @Component({
   selector: 'app-modelo',
@@ -11,64 +13,148 @@ import { FormBuilder } from '@angular/forms';
 export class ModeloComponent implements OnInit {
 
   public modelos;
-  public displayedColumns: string[] = ["descricao", "marca", "remover"];
+  public marcas;
+  public displayedColumns: string[] = ["descricao", "marca", "alterar", "remover"];
   public dataSource;
+  public cadastroModeloForm: FormGroup;
   
   @ViewChild(MatTable) table: MatTable<any>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
 
   constructor(
-    private fb: FormBuilder
+    private formBuilder: FormBuilder,
+    private modeloService: ModeloService,
+    private marcaService: MarcaService
   ) { }
 
   ngOnInit(): void {
-    this.modelos = [
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      },
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      },
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      },
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      },
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      },
-      {
-        id: "fddg-32df-g345434-hhfgdh",
-        descricao: "Modelo ABC",
-        marca: "Marca 123"
-      }
-    ];
+    this.cadastroModeloForm = this.formBuilder.group({
+      id: [null, []],
+      nome: ["", [Validators.required]],
+      marca: this.formBuilder.group({
+        id: [null, [Validators.required]]
+      }),
+      ativo: [true, [Validators.required]]
+    });
 
-    this.dataSource = new MatTableDataSource<PeriodicElement>(this.modelos);
-    this.dataSource.paginator = this.paginator;
+    this.listarTodos();
   }
+
 
   private renderTable() {
     this.dataSource = new MatTableDataSource<PeriodicElement>(this.modelos);
     this.table.renderRows();
   }
 
+
+  public filtrarPorNome(nome: string) {
+    this.dataSource.filterPredicate = (data: PeriodicElement, filter: string) => {
+      return data.nome.toLocaleLowerCase().includes(filter);
+    };
+
+    this.dataSource.filter = nome.trim().toLocaleLowerCase();
+  }
+
+
+  public filtrarPorMarca(marca: string) {
+    this.dataSource.filterPredicate = (data: PeriodicElement, filter: string) => {
+      return data.marca.nome.toLocaleLowerCase().includes(filter);
+    };
+
+    this.dataSource.filter = marca.trim().toLocaleLowerCase();
+  }
+
+
+  public cadastrarOuAlterar() {
+    if (this.cadastroModeloForm.valid) {
+      if (this.cadastroModeloForm.get("id").value) {
+        this.modeloService.alterar(this.cadastroModeloForm.value).subscribe(res => {
+          this.listarTodos();
+          this.resetForm();
+          this.dismissModal();
+        }, err => {});
+      }
+      else {
+        this.modeloService.cadastrar(this.cadastroModeloForm.value).subscribe(res => {
+          this.listarTodos();
+          this.resetForm();
+          this.dismissModal();
+        }, err => {});
+      }
+    }
+  }
+
+
+  public alterar(element) {
+    this.cadastroModeloForm.get("id").setValue(element.id);
+    this.cadastroModeloForm.get("nome").setValue(element.nome);
+    this.cadastroModeloForm.get("marca").get("id").setValue(element.marca.id);
+    this.cadastroModeloForm.get("ativo").setValue(element.ativo);
+  }
+
+
+  public desativar(id: number) {
+    this.modeloService.desativar(id).subscribe(res => {
+      this.listarTodos();
+    }, err => {});
+  }
+
+
+  public buscar(id: number) {
+    this.modeloService.buscar(id).subscribe(res => {}, err => {});
+  }
+
+
+  public listarTodos() {
+    this.modeloService.listarTodos().subscribe((data: any[]) => {
+      this.modelos = data.filter(modelo => {
+        if (modelo.ativo) {
+          return modelo;
+        }
+      });
+      this.dataSource = new MatTableDataSource<PeriodicElement>(this.modelos);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.carregarDados();
+  }
+
+
+  public dismissModal() {
+    document.getElementById("fecharModal").click();
+  }
+
+
+  public resetForm() {
+    this.cadastroModeloForm = this.formBuilder.group({
+      id: [null, []],
+      nome: ["", [Validators.required]],
+      marca: this.formBuilder.group({
+        id: [null, [Validators.required]]
+      }),
+      ativo: [true, [Validators.required]]
+    });
+  }
+
+
+  public carregarDados() {
+    this.marcaService.listarTodos().subscribe((data: any[]) => {
+      this.marcas = data.filter(marca => {
+        if (marca.ativo) {
+          return marca;
+        }
+      });
+    });
+  }
+
+
 }
 
 export interface PeriodicElement {
   id: string,
-  descricao: string,
-  marca: string
+  nome: string,
+  marca: {
+    id: number,
+    nome: string
+  }
 }
--- Almoxarifado/pi-almoxarifado/src/app/modules/cadastros/modelo/modelo.service.ts
+++ Almoxarifado/pi-almoxarifado/src/app/modules/cadastros/modelo/modelo.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModeloService {
 
-  constructor() { }
+  private urlBase = `${ environment.urlBase }/modelo`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public cadastrar(dados: any) {   
+    return this.http.post(this.urlBase, dados);
+  }
+
+  public alterar(dados: any) {   
+    return this.http.put(this.urlBase, dados);
+  }
+
+  public desativar(id: number) {   
+    return this.http.delete(`${ this.urlBase }/${ id }`);
+  }
+
+  public buscar(id: number) {   
+    return this.http.get(`${ this.urlBase }/${ id }`);
+  }
+
+  public listarTodos() {   
+    return this.http.get(this.urlBase);
+  }
+
 }
